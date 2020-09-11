70f3d036452e63c7345e303b683e07e1e45adfbf
Adam Stammer <adam.stammer@go.winona.edu>
2020-09-11 09:44:02 -0500

    test conflict

--- BoardGame/BoardGame/src/edu/wsu/se/Main.java
+++ BoardGame/BoardGame/src/edu/wsu/se/Main.java
@@ -4,8 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Howdy!");
-		while (true) {
-			
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Howdy!");
 		}
 	}
 
