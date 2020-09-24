885531fd9e48f37de7e6df2c9d12bc2e245c258a
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2020-09-24 09:11:54 -0500

    Update Player.java
    Moved hand to Player from Game

--- BoardGame/BoardGame/src/edu/wsu/se/Player.java
+++ BoardGame/BoardGame/src/edu/wsu/se/Player.java
@@ -1,24 +1,51 @@
 package edu.wsu.se;
 
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Player {
 
-	int playerNumber;
-	int wins = 0;
-	double IPaddress;
-////////////////////////////////Constructor
-	public void Player(int playerNum, int IP)
+	Set<Integer> hand = new TreeSet<Integer>();
+	int number;
+	int wins;
+	
+	double ip_address;
+	
+//////////////////////////////////////////////// Constructor
+	public void Player(int number, double ip_address)
 	{
-		playerNumber = playerNum;
-		IPaddress = IP;
+		this.number = number;
+		this.ip_address = ip_address;
+		wins = 0;
+		/*
+		Random rand = new Random();
+			for(int i = 0; i < 3; i++)
+			{
+				hand.add(rand.nextInt(20)+1);
+			}	
+			*/
+		
+	}
+	public void addNumber(int number) 
+	{
+		hand.add(number);
+	}
+	/////////////////////////////////////////////// Setters and Getters
+	public int getNumber() {
+		return number;
 	}
 
-////////////////////////////////Getters and Setters
-	public int getPlayerNumber() {
-		return playerNumber;
+	public void setNumber(int number) {
+		this.number = number;
 	}
 
-	public void setPlayerNumber(int playerNumber) {
-		this.playerNumber = playerNumber;
+	public double getIp_address() {
+		return ip_address;
+	}
+
+	public void setIp_address(double ip_address) {
+		this.ip_address = ip_address;
 	}
 
 	public int getWins() {
@@ -28,14 +55,24 @@ public class Player {
 	public void setWins(int wins) {
 		this.wins = wins;
 	}
-
-	public double getIPaddress() {
-		return IPaddress;
+	public Set<Integer> getHand() {
+		return hand;
 	}
-
-	public void setIPaddress(double iPaddress) {
-		IPaddress = iPaddress;
+	public void setHand(Set<Integer> hand) {
+		this.hand = hand;
 	}
-
+	public String displayHand()
+	{
+		
+		String value = "[ ";
+		for (int i : hand) 
+		{
+		    value += (" "+ i);
+		}
+		value += " ]";
+		return value;
+	}
+	
+	
 	
 }
