da070426ae483f24638a0c0fd8e06c664e0810ec
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2020-09-26 10:59:26 -0500

    Update Game.java
    Organized and created single matrix instead of 4 seperate ones

--- BoardGame/BoardGame/src/edu/wsu/se/Game.java
+++ BoardGame/BoardGame/src/edu/wsu/se/Game.java
@@ -6,205 +6,206 @@ import java.util.Random;
 import java.util.Set;
 import java.util.TreeSet;
 public class Game {
-
-	boolean[] p1Matrix = new boolean[4];
-	boolean[] p2Matrix = new boolean[4];
-	boolean[] p3Matrix = new boolean[4];
-	boolean[] p4Matrix = new boolean[4];
-	
-	boolean gameEnd = false;
-	int turnNumber = 0;
-	int whoseTurn = 1;
-	int whomWon = -1;
-	boolean alreadyWon = false;
-	
+/*
+ * Contains
+ * (A)Game Constructor
+ * (B)Matrix Display
+ * (C)Update Matrix
+ * (D)New Turn
+ * (E)Check If Wins
+ * (F)Game End
+ * (G)Increment Turn
+ * (H)Misc. Getters and Setters
+ */
 	Set<Integer> player1 = new TreeSet<Integer>();
 	Set<Integer> player2 = new TreeSet<Integer>();
 	Set<Integer> player3 = new TreeSet<Integer>();
 	Set<Integer> player4 = new TreeSet<Integer>();
-	
+	boolean gameEnd = false;
+	int turnNumber = 1;
+	int whoseTurn = 1;
+	int whomWon = -1;
+	boolean alreadyWon = false;
+	boolean[][] playerMatrix = new boolean[4][4];
+////////////////////////////////////////////////////////////////(A)Game Constructor
 	public Game(Set<Integer> hand, Set<Integer> hand2, Set<Integer> hand3, Set<Integer> hand4) {
 		player1 = hand;
 		player2 = hand2;
 		player3 = hand3;
 		player4 = hand4;
+		playerMatrix[0][0] = true;
+		playerMatrix[1][1] = true;
+		playerMatrix[2][2] = true;
+		playerMatrix[3][3] = true;
 	}
-	
-	public void startGame()
-	{
-		Random rand = new Random();
-		for(int i = 0; i < 3; i++)
-		{
-			p1Matrix[0] = true;
-			p2Matrix[1] = true;
-			p3Matrix[2] = true;
-			p4Matrix[3] = true;
+///////////////////////////////////////////////////(B)Matrix Display
+	public String displayMatrix(){
+		String matrix = "P1: [";
+		for(int i = 0; i < 4; i++){
+			matrix += playerMatrix[0][i] + " ";	
 		}
+		matrix += "]\n";
+		matrix += "P2: [";
+		for(int i = 0; i < 4; i++){
+			matrix += playerMatrix[1][i] + " ";	
+		}
+		matrix += "]\n";
+		matrix += "P3: [";
+		for(int i = 0; i < 4; i++){
+			matrix += playerMatrix[2][i] + " ";	
+		}
+		matrix += "]\n";
+		matrix += "P4: [";
+		for(int i = 0; i < 4; i++){
+			matrix += playerMatrix[3][i] + " ";	
+		}
+		matrix += "]\n";
+		return matrix;
 	}
-	
-	public void newTurn(int player, int wanted,Set<Integer> p1,Set<Integer> p2,Set<Integer> p3,Set<Integer> p4)
-	{
-		
+///////////////////////////////////////////////////(C)Update Matrix
+	public void updateMatrix(Set<Integer> p){
 		switch (whoseTurn) {
         case 1:  whoseTurn = 1;
+	        if(p.containsAll(player2)){
+	    		playerMatrix[0][1] = true;
+	    	}else{
+	        	playerMatrix[0][1] = false;
+	        }
+	    	if(p.containsAll(player3)){	
+	    		playerMatrix[0][2] = true;
+	    	}else{
+	    		playerMatrix[0][2] = false;
+	        }
+	    	if(p.containsAll(player4)){
+	    		playerMatrix[0][3] = true;
+	    	}else{
+	    		playerMatrix[0][3] = false;
+	        }
+                 break;
+        case 2:  whoseTurn = 2;
+	        if(p.containsAll(player1)){	
+	    		playerMatrix[1][0] = true;
+	    	}else{
+	    		playerMatrix[1][0] = false;
+	        }
+	    	if(p.containsAll(player3)){
+	    		playerMatrix[1][2] = true;
+	    	}else{	
+	    		playerMatrix[1][2] = false;
+	        }
+	    	if(p.containsAll(player4)){
+	    		playerMatrix[1][3] = true;
+	    	}else{
+	    		playerMatrix[1][3] = false;
+	        }
+	                 break;
+        case 3:  whoseTurn = 3;
+	        if(p.containsAll(player1)){
+	    		playerMatrix[2][0] = true;
+	    	}else{
+	    		playerMatrix[2][0] = false;
+	        }
+	    	if(p.containsAll(player2)){
+	    		playerMatrix[2][1] = true;
+	    	}else{	
+	    		playerMatrix[2][1] = false;
+	        }
+	    	if(p.containsAll(player4)){
+	    		playerMatrix[2][3] = true;
+	    	}else{	
+	    		playerMatrix[2][3] = false;
+	        }
+                 break;
+        case 4:  whoseTurn = 4;
+	        if(p.containsAll(player1)){
+	    		playerMatrix[3][0] = true;
+	    	}else{
+	    		playerMatrix[3][0] = false;
+	        }
+	    	if(p.containsAll(player2)){
+	    		playerMatrix[3][1] = true;
+	    	}else{
+	    		playerMatrix[3][1] = false;
+	        }
+	    	if(p.containsAll(player3)){
+	    		playerMatrix[3][2] = true;
+	    	}else{
+	    		playerMatrix[3][2] = false;
+	        }
+                 break;
+        default: whoseTurn = -1;
+                 break;
+		}	
+	}
+////////////////////////////////////////////////////////////////(D)New Turn
+public void newTurn(int player, int wanted,Set<Integer> p){
+	switch (whoseTurn) {
+		case 1:  whoseTurn = 1;
 			player1.add(wanted);
-			updateMatrix(p1,p2,p3,p4);
+			updateMatrix(p);
 			checkWin();
 			checkgameEnd();
 			whoseTurn = 2;
 			break;
-        case 2:  whoseTurn = 2;	
+		case 2:  whoseTurn = 2;	
 			player2.add(wanted);
-			updateMatrix(p1,p2,p3,p4);
+			updateMatrix(p);
 			checkWin();
 			checkgameEnd();
 			whoseTurn = 3;
 			break;
-        case 3:  whoseTurn = 3;	
+		case 3:  whoseTurn = 3;	
 			player3.add(wanted);
-			updateMatrix(p1,p2,p3,p4);
+			updateMatrix(p);
 			checkWin();
 			checkgameEnd();
 			whoseTurn = 4;
 			break;
-        case 4:  whoseTurn = 4;	
+		case 4:  whoseTurn = 4;	
 			player4.add(wanted);
-			updateMatrix(p1,p2,p3,p4);
+			updateMatrix(p);
 			checkWin();
 			checkgameEnd();
 			incrementTurn();
 			whoseTurn = 1;
 			break;
-			}
-		
+		}
 	}
-	public void updateMatrix(Set<Integer> p1,Set<Integer> p2,Set<Integer> p3,Set<Integer> p4)
-	{
-		switch (whoseTurn) {
-        case 1:  whoseTurn = 1;
-	        if(p1.containsAll(p2)){
-	    		p1Matrix[1] = true;
-	    	}
-	        else{
-	        	p1Matrix[1] = false;
-	        }
-	    	if(p1.containsAll(p3)){
-	    		p1Matrix[2] = true;	    		
-	    	}
-	    	else{
-	    		p1Matrix[2] = false;	    		
-	        }
-	    	if(p1.containsAll(p4)){
-	    		p1Matrix[3] = true;	    		
-	    	}
-	    	else{
-	    		p1Matrix[3] = false;	
-	        }
-                 break;
-        case 2:  whoseTurn = 2;
-	        if(p2.containsAll(p1)){
-	    		p2Matrix[0] = true;
-	    	}
-	        else
-	        {
-	    		p2Matrix[0] = false;
-	        }
-	    	if(p2.containsAll(p3)){
-	    		p2Matrix[2] = true;
-	    	}
-	    	else
-	        {
-	    		p2Matrix[2] = false;
-	        }
-	    	if(p2.containsAll(p4)){
-	    		p2Matrix[3] = true;
-	    	}
-	    	else
-	        {
-	    		p2Matrix[3] = false;
-	        }
-	                 break;
-        case 3:  whoseTurn = 3;
-	        if(p3.containsAll(p1)){
-	    		p3Matrix[0] = true;
-	    	}else
-	        {
-	    		p3Matrix[0] = false;
-	        }
-	    	if(p3.containsAll(p2)){
-	    		p3Matrix[1] = true;
-	    	}else
-	        {
-	    		p3Matrix[1] = false;
-	        }
-	    	if(p3.containsAll(p4)){
-	    		p3Matrix[3] = true;
-	    	}else
-	        {
-	    		p3Matrix[3] = false;
-	        }
-                 break;
-        case 4:  whoseTurn = 4;
-	        if(p4.containsAll(p1)){
-	    		p4Matrix[0] = true;
-	    	}else
-	        {
-	    		p4Matrix[0] = false;
-	        }
-	    	if(p4.containsAll(p2)){
-	    		p4Matrix[1] = true;
-	    	}else
-	        {
-	    		p4Matrix[1] = false;
-	        }
-	    	if(p4.containsAll(p3)){
-	    		p4Matrix[2] = true;
-	    	}else
-	        {
-	    		p4Matrix[2] = false;
-	        }
-                 break;
-        default: whoseTurn = -1;
-                 break;
-    }
-		
-	}
-	public void checkWin()
-	{
-		if(p1Matrix[1] == true & p1Matrix[2] == true & p1Matrix[3] == true)
-		{
+///////////////////////////////////////////////////(E)Check If Wins
+	public void checkWin(){
+		if(playerMatrix[0][1] == true & playerMatrix[0][2] == true & playerMatrix[0][3] == true){
 			whomWon = 1;
 			gameEnd = true;
 		}
-		if(p2Matrix[0] == true & p2Matrix[2] == true & p2Matrix[3] == true)
-		{
+		if(playerMatrix[1][0] == true & playerMatrix[1][2] == true & playerMatrix[1][3] == true){
 			whomWon = 2;
 			gameEnd = true;
 		}
-		if(p3Matrix[0] == true & p3Matrix[1] == true & p3Matrix[3] == true)
-		{
+		if(playerMatrix[2][0] == true & playerMatrix[2][1] == true & playerMatrix[2][3] == true){
 			whomWon = 3;
 			gameEnd = true;
 		}
-		if(p4Matrix[0] == true & p4Matrix[1] == true & p4Matrix[2] == true)
-		{
+		if(playerMatrix[3][0] == true & playerMatrix[3][1] == true & playerMatrix[3][2] == true){
 			whomWon = 4;
 			gameEnd = true;
 		}
-		
 	}
-	public void checkgameEnd()
+///////////////////////////////////////////////////(F)Game End
+	public boolean checkgameEnd()
 	{
-		if(gameEnd == true)
-		{
+		if(gameEnd == true){
 			if(whoseTurn != -1 & alreadyWon == false) {
-				System.out.println("Player " + whoseTurn + " has won on turn " +turnNumber);
+				System.out.println("Player " + whoseTurn + " has won on turn " +turnNumber+"!");
 				alreadyWon = true;
+				displayMatrix();
 			}
-				if(whoseTurn == -1)
-					System.out.println("The game has run out of turns");
+			else if(whoseTurn == -1) {
+				System.out.println("The game has run out of turns");
+			}
 		}
+		return gameEnd;
 	}
+///////////////////////////////////////////////////(G)Increment Turn
 	public void incrementTurn()
 	{
 		turnNumber++;
@@ -212,126 +213,44 @@ public class Game {
 			whoseTurn = -1;
 			gameEnd = true;
 		}
-	}	
-	///////////////////////////////////////////////////Hand and Matrix Display
-	public String displayMatrix()
-	{
-		String matrix = "P1: [";
-		for(int i = 0; i < 4; i++)
-		{
-			matrix += p1Matrix[i] + " ";	
-		}
-		matrix += "]\n";
-		matrix += "P2: [";
-		for(int i = 0; i < 4; i++)
-		{
-			matrix += p2Matrix[i] + " ";	
-		}
-		matrix += "]\n";
-		matrix += "P3: [";
-		for(int i = 0; i < 4; i++)
-		{
-			matrix += p3Matrix[i] + " ";	
-		}
-		matrix += "]\n";
-		matrix += "P4: [";
-		for(int i = 0; i < 4; i++)
-		{
-			matrix += p4Matrix[i] + " ";	
-		}
-		matrix += "]\n";
-		
-		return matrix;
 	}
-///////////////////////////////////////////////////Getters and Setters
-	public int getTurnNumber()
-	{
+/////////////////////////////////////////////////////(H)Getters and Setters
+/////////////////////////////////////////////////////Turn Number
+	public int getTurnNumber(){
 		return turnNumber;
 	}
-	public int getwhoseTurn()
-	{
-		return whoseTurn;
-	}
-	public boolean[] getP1Matrix() {
-		return p1Matrix;
-	}
-	public boolean[] getP2Matrix() {
-		return p2Matrix;
-	}
-	public boolean[] getP3Matrix() {
-		return p3Matrix;
-	}
-
-	public void setP3Matrix(boolean[] p3Matrix) {
-		this.p3Matrix = p3Matrix;
-	}
-
-	public boolean[] getP4Matrix() {
-		return p4Matrix;
-	}
-
-	public void setP4Matrix(boolean[] p4Matrix) {
-		this.p4Matrix = p4Matrix;
-	}
-
-	public boolean isGameEnd() {
-		return gameEnd;
-	}
-
-	public void setGameEnd(boolean gameEnd) {
-		this.gameEnd = gameEnd;
-	}
-
-	public int getWhoseTurn() {
-		return whoseTurn;
-	}
-
-	public void setWhoseTurn(int whoseTurn) {
-		this.whoseTurn = whoseTurn;
-	}
-
-	public int getWhomWon() {
-		return whomWon;
-	}
-
-	public void setWhomWon(int whomWon) {
-		this.whomWon = whomWon;
-	}
-
-	public void setP1Matrix(boolean[] p1Matrix) {
-		this.p1Matrix = p1Matrix;
-	}
-
-	public void setP2Matrix(boolean[] p2Matrix) {
-		this.p2Matrix = p2Matrix;
-	}
-
 	public void setTurnNumber(int turnNumber) {
 		this.turnNumber = turnNumber;
 	}
-	public void startGame2()
-	{
-			p1Matrix[0] = true;
-			p2Matrix[1] = true;
-			p3Matrix[2] = true;
-			p4Matrix[3] = true;
-		
-			player1.add(1);
-			player2.add(1);
-			player3.add(1);
-			player4.add(1);
-		
-			player1.add(2);
-			player2.add(2);
-			player3.add(2);
-			player4.add(2);
-			
-			player1.add(3);
-			player2.add(4);
-			player3.add(5);
-			player4.add(6);
+/////////////////////////////////////////////////////Whose Turn
+	public int getWhoseTurn() {
+		return whoseTurn;
 	}
-
-
+	public void setWhoseTurn(int whoseTurn) {
+		this.whoseTurn = whoseTurn;
+	}
 	
+/////////////////////////////////////////////////////Game End
+	public boolean isGameEnd() {
+		return gameEnd;
+	}
+	public void setGameEnd(boolean gameEnd) {
+		this.gameEnd = gameEnd;
+	}
+	
+/////////////////////////////////////////////////////Who Won
+	public int getWhomWon() {
+		return whomWon;
+	}
+	public void setWhomWon(int whomWon) {
+		this.whomWon = whomWon;
+	}
+	
+/////////////////////////////////////////////////////Matrix
+	public boolean[][] getPlayerMatrix() {
+		return playerMatrix;
+	}
+	public void setPlayerMatrix(boolean[][] playerMatrix) {
+		this.playerMatrix = playerMatrix;
+	}	
 }
