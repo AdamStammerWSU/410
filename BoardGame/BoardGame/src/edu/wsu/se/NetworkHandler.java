751424685898da7e8f4b963e26a9a976c101ca15
AdamStammerWSU <adam.stammer@go.winona.edu>
2020-09-28 18:13:16 -0500

    new networkhandler ready to integrate

--- BoardGame/BoardGame/src/edu/wsu/se/NetworkHandler.java
+++ BoardGame/BoardGame/src/edu/wsu/se/NetworkHandler.java
@@ -11,53 +11,150 @@ public class NetworkHandler {
 	boolean isServer = false;
 	int port = 8080;
 	String ip = "";
-	
+
 	int numberOfClients = 0;
 
-	Socket socket = null;
-	ServerSocket serverSocket = null;
-	DataInputStream[] inputStreams = null;
-	DataOutputStream[] outputStreams = null;
+	ServerSocket serverSoc = null;
+	Server server = null;
+	Client[] clients = null;
 
 	public NetworkHandler(boolean isServer, String ip, int port) {
 		this.isServer = isServer;
 		this.ip = ip;
 		this.port = port;
 
-		inputStreams = new DataInputStream[3];
-		outputStreams = new DataOutputStream[3];
-
 		if (isServer) {
-			// setup server
+			//setup server
+			clients = new Client[3];
+			// start server
 			try {
-				serverSocket = new ServerSocket(port);
+				serverSoc = new ServerSocket(port);
 			} catch (IOException e) {
-				System.out.println("Failed to start the Server");
+				System.out.println("Failed to start server");
 				e.printStackTrace();
 				System.exit(0);
 			}
-			System.out.println("Server Started");
 
-		} else {
-			// setup client
-			try {
-				System.out.println("Trying to connect to " + ip);
-				socket = new Socket(ip, port);
-				System.out.println("Connected to server");
-				
-				inputStreams[numberOfClients++] = new DataInputStream(System.in);
-				
-				
-			} catch (IOException e) {
-				System.out.println("Failed to connect to client");
-				e.printStackTrace();
-				System.exit(0);
+			// wait for clients to connect
+			while (numberOfClients < 3) {
+				clients[numberOfClients] = new Client(numberOfClients);
+				clients[numberOfClients++].waitForConnection(serverSoc);
 			}
+		} else {
+			// client setup
+			server = new Server(ip, port);
+			server.connectToServer();
 		}
 	}
 
 	public NetworkHandler(boolean isServer, int port) {
 		this(isServer, "localhost", port);
+	}
+
+	public class Client {
+
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+
+		Socket socket = null;
+
+		int clientNumber = 0;
+
+		public Client(int number) {
+			clientNumber = number;
+		}
+
+		public void waitForConnection(ServerSocket soc) {
+			try {
+				socket = soc.accept();
+				dos = new DataOutputStream(socket.getOutputStream());
+				dis = new DataInputStream(socket.getInputStream());
+				System.out.println("Client Connected!");
+			} catch (IOException e) {
+				System.out.println("Client Failed To Connect");
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+		public String readUTF() {
+			System.out.println("Reading Message From (" + clientNumber + ")");
+			String s = "";
+			try {
+				s = dis.readUTF();
+			} catch (IOException e) {
+				System.out.println("Failed to read message");
+				e.printStackTrace();
+				System.exit(0);
+			}
+			return s;
+		}
+
+		public void writeUTF(String s) {
+			System.out.println("Writing Message to (" + clientNumber + ")");
+			try {
+				dos.writeUTF(s);
+			} catch (IOException e) {
+				System.out.println("Failed to write message");
+				e.printStackTrace();
+			}
+			System.out.println("message written");
+		}
+
+	}
+
+	public class Server {
+
+		DataInputStream dis = null;
+		DataOutputStream dos = null;
+
+		Socket socket = null;
+
+		String ip = "";
+		int port = 8080;
+
+		public Server(String ip, int port) {
+			this.ip = ip;
+			this.port = port;
+		}
+
+		public void connectToServer() {
+			try {
+				System.out.println("Trying to connect to server at " + ip + ":" + port);
+				socket = new Socket(ip, port);
+				dos = new DataOutputStream(socket.getOutputStream());
+				dis = new DataInputStream(socket.getInputStream());
+				System.out.println("Connected To Server");
+			} catch (IOException e) {
+				System.out.println("Failed to connect to server");
+				e.printStackTrace();
+				System.exit(0);
+			}
+		}
+
+		public String readUTF() {
+			System.out.println("Reading Message From Server");
+			String s = "";
+			try {
+				s = dis.readUTF();
+			} catch (IOException e) {
+				System.out.println("Failed to read message");
+				e.printStackTrace();
+				System.exit(0);
+			}
+			return s;
+		}
+
+		public void writeUTF(String s) {
+			System.out.println("Writing Message to Server");
+			try {
+				dos.writeUTF(s);
+			} catch (IOException e) {
+				System.out.println("Failed to write message");
+				e.printStackTrace();
+			}
+			System.out.println("message written");
+		}
 	}
 
 }
