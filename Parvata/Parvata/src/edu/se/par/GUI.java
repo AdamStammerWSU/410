c0163acfe669e6b23f73512db003a49ff20dd465
AdamStammerWSU <adam.stammer@go.winona.edu>
2020-10-29 10:21:43 -0500

    initialize gui test

--- Parvata/Parvata/src/edu/se/par/GUI.java
+++ Parvata/Parvata/src/edu/se/par/GUI.java
@@ -1,9 +1,24 @@
 package edu.se.par;
 
+import javax.swing.JFrame;
+
 public class GUI {
 	
 	public static void main(String[] args) {
 		System.out.println("Howdy");
+		GUI gui = new GUI();
+	}
+	
+	
+	
+	JFrame frame;
+	
+	
+	public GUI() {
+		frame = new JFrame();
+		frame.setSize(500, 500);
+		frame.setTitle("This is a test");
+		frame.setVisible(true);
 	}
 
 }
