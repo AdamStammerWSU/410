adf54d19842e266b1b709ac055da8f734e748ac6
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2020-11-02 15:13:27 -0600

    Update FileManager.java

--- Parvata/Parvata/src/edu/se/par/FileManager.java
+++ Parvata/Parvata/src/edu/se/par/FileManager.java
@@ -3,5 +3,23 @@ package edu.se.par;
 public class FileManager {
 
 	
+	public void saveImage() {
+		
+	}
+	public void loadImage() {
+		
+	}
+	public void saveLayout() {
+		
+	}
+	public void loadLayout() {
+		
+	}
+	public void PNGtoPDF() {
+		
+	}
+	public void PDFtoPNG() {
+		
+	}
 	
 }
