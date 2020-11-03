7fbfb4223159d805e315779e7474016393fd97cd
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2020-11-03 11:47:18 -0600

    Added jar files to file path

--- Parvata/Parvata/src/edu/se/par/FileManager.java
+++ Parvata/Parvata/src/edu/se/par/FileManager.java
@@ -1,25 +1,39 @@
 package edu.se.par;
 
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
+
+
+
 
 public class FileManager {
 
-	//Image Load/Save
+	//Image Save
 	static void saveImage(BufferedImage page, String fileLocation) {
-		
+		//unused
 		page = null;
-		fileLocation = "";
+		//unused
+		
+		
+		
 		
 		
 	}
-	
-	static BufferedImage loadImage(String fileLocation) {
+	//Image Load
+	static BufferedImage loadImage(String fileLocation) throws IOException {
 		
 		
+		
+		//unused
 		BufferedImage image = null;
-		
-		
 		return image;
+		//unused
 	}
 	
 	//Image Conversions
@@ -27,7 +41,14 @@ public class FileManager {
 		
 	}
 	
-	static void PDFtoPNG() {
+	static void PDFtoPNG(String fileLocation) throws IOException {
+		
+		File file = new File(fileLocation);
+		PDDocument document = PDDocument.load(file);
+		PDFRenderer renderer = new PDFRenderer(document);
+		BufferedImage image = renderer.renderImage(0);		//index
+	    ImageIO.write(image, "JPEG", new File(fileLocation));
+	    document.close();
 		
 	}
 	
