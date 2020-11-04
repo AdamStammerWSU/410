cd5f6beff80c5371827e4605deaba364832104bb
Stepher67 <57917055+Stepher67@users.noreply.github.com>
2020-11-04 17:35:20 -0600

    Create Main.java

---
+++ Parvata/Parvata/src/edu/se/par/Main.java
@@ -0,0 +1,15 @@
+package edu.se.par;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Howdy");
+		GUI gui = new GUI();
+		gui.setSize(500, 500);
+		gui.setLocationRelativeTo(null); // this isn't working either, for some reason...
+		gui.setVisible(true);
+
+	}
+
+}
