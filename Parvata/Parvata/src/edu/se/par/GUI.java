4ddca40eee5570e6cb8116c9054ae1a435f55bfd
Stepher67 <57917055+Stepher67@users.noreply.github.com>
2020-11-04 19:19:17 -0600

    Small changes to visual and action listeners for buttons

--- Parvata/Parvata/src/edu/se/par/GUI.java
+++ Parvata/Parvata/src/edu/se/par/GUI.java
@@ -2,10 +2,14 @@ package edu.se.par;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.LayoutManager;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -29,7 +33,7 @@ public class GUI extends JFrame implements ActionListener {
 	
 	// On-screen components for panelLeft
 	JLabel enterLayout = new JLabel("Enter Page Layout");
-	JTextArea layout = new JTextArea("EXAMPLE LAYOUT HERE", 20, 15);
+	JTextArea layout = new JTextArea("EXAMPLE LAYOUT HERE", 10, 15);
 	
 	// On-screen components for panelRight
 	JLabel tba = new JLabel("panel TBA");
@@ -58,16 +62,21 @@ public class GUI extends JFrame implements ActionListener {
 		
 		title.setFont(titleFont);
 		
-		panelLeft.setLayout((LayoutManager) new BoxLayout(panelLeft, BoxLayout.Y_AXIS));
+		panelTop.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
+		
+		panelLeft.setLayout((LayoutManager) new FlowLayout());
+		panelLeft.setBorder(BorderFactory.createEmptyBorder(0, 10, 5, 0));
+		
 		panelRight.setLayout((LayoutManager) new BoxLayout(panelRight, BoxLayout.Y_AXIS));
 		
 		
 		// Adds components to the top panel
 		panelTop.add(title);
-		panelTop.setPreferredSize(new Dimension(500, 100)); // I don't know why none of these are working or what makes them different from the last two projects
+		panelTop.setPreferredSize(new Dimension(500, 80)); // I don't know why none of these are working or what makes them different from the last two projects
 		
 		// Adds components to the left panel
 		panelLeft.add(enterLayout);
+		panelLeft.add(Box.createRigidArea(new Dimension(0, 20)));
 		panelLeft.add(layout);
 		panelLeft.setPreferredSize(new Dimension(300, 300));
 		
@@ -83,12 +92,10 @@ public class GUI extends JFrame implements ActionListener {
 		panelBottom.add(newFilePath);
 		panelBottom.setPreferredSize(new Dimension(500, 100));
 		
-		
-		//openFile.addActionListener(this);
-		//fc = new JFileChooser();
-		//saveFile.addActionListener(this);
-		
-		
+		impose.addActionListener(this);
+		openFile.addActionListener(this);
+		fc = new JFileChooser();
+		saveFile.addActionListener(this);
 		
 		// Panels' position on frame
 		add(panelTop, BorderLayout.NORTH);
@@ -98,7 +105,7 @@ public class GUI extends JFrame implements ActionListener {
 		pack();
 		
 	}
-
+	
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -108,11 +115,17 @@ public class GUI extends JFrame implements ActionListener {
 
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				// Do stuff
+				System.out.println("You picked a file");
+			} else {
+				// User choose cancel
 			}
 		} else if (e.getSource() == saveFile){
 			// Do stuff
+			System.out.println("You clicked Save File");
+			
 		} else if (e.getSource() == impose) {
 			// Do stuff
+			System.out.println("You clicked Impose");
 		}
 	}
 }
