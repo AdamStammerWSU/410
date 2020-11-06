d55461b0b32b47ae1d710b6366544155da733424
AkinTemaLopez <71061240+AkinTemaLopez@users.noreply.github.com>
2020-11-06 09:23:40 -0600

    Update Layout.java

--- Parvata/Parvata/src/edu/se/par/Layout.java
+++ Parvata/Parvata/src/edu/se/par/Layout.java
@@ -2,4 +2,37 @@ package edu.se.par;
 
 public class Layout {
 
+	int inputPageCount;
+	int outputPageCount;
+	String layout;
+	String layoutArray [][];
+	
+	public Layout() {
+		
+	}
+	
+	public int getInputPageCount()
+	{
+		return inputPageCount;	
+	}
+	
+	public int getOutputPageCount()
+	{
+		return outputPageCount;
+	}
+	
+	public String[][] getLayout()
+	{
+		return layoutArray;
+	}
+	
+	public String getLayoutString()
+	{
+		return layout;
+	}
+	
+	public void saveLayout()
+	{
+		
+	}
 }
