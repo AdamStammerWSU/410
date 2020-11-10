d85d2d21aed67b079700a2e856bf617ba264db05
HenryWeber <70726490+HenryWeber@users.noreply.github.com>
2020-11-10 10:11:12 -0600

    Update FileManager.java

--- Parvata/Parvata/src/edu/se/par/FileManager.java
+++ Parvata/Parvata/src/edu/se/par/FileManager.java
@@ -2,12 +2,20 @@ package edu.se.par;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 import javax.imageio.ImageIO;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.apache.pdfbox.rendering.ImageType;
 import org.apache.pdfbox.rendering.PDFRenderer;
+import org.apache.pdfbox.tools.PDFToImage;
+import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
 
 
@@ -28,6 +36,10 @@ class FileManager {
 	//Image Load
 	static BufferedImage loadImage(String fileLocation) throws IOException {
 		
+		
+			
+		//PDImageXObject pdImage = PDImageXObject.createFromFile(fileLocation, document);
+		//contentStream.drawImage(pdImage, 20f, 20f); 
 		//unused
 		BufferedImage page = null;
 		return page;
@@ -41,6 +53,17 @@ class FileManager {
 	
 	static void PDFtoPNG(String fileLocation) throws IOException {
 		
+		PDDocument document = PDDocument.load(new File(fileLocation));
+		PDFRenderer pdfRenderer = new PDFRenderer(document);
+		
+		for (int page = 0; page < document.getNumberOfPages(); ++page)
+		{ 
+		    BufferedImage buffimage = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
+		    ImageIOUtil.writeImage(buffimage, "file-" + (page+1) + ".png", 300);
+		}
+		document.close();
+		
+		/*
 		//Works inside terminal
 		//ImageMagik
 		//convert -density 300 file:///Users/kd7933mc/Desktop/th.PDF -resize 25% a.png
@@ -50,6 +73,7 @@ class FileManager {
 		String command = "CONVERT file:///Users/kd7933mc/Desktop/th.PDF -quality 100 F:\th4.PNG";
 		Runtime r = Runtime.getRuntime(); 
         r.exec(command); 
+        */
 	}
 	
 	
