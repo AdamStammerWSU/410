aea169578bc0b34cde8c14bfdd7e0ca58103282a
AdamStammerWSU <adam.stammer@go.winona.edu>
2020-11-09 21:57:59 -0600

    signature loops

--- Parvata/Parvata/src/edu/se/par/Signature.java
+++ Parvata/Parvata/src/edu/se/par/Signature.java
@@ -1,19 +1,22 @@
 package edu.se.par;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Signature {
 
 	Layout layout;
 	int inputPageOffset, outputPageOffset;
 	InputPage inputPages[];
-	OutputPage outputPage;
+	OutputPage[] outputPages;
 	int test = 0;
 
 	public Signature(Layout layout, int inputPageOffset, int outputPageOffset) {
 		this.layout = layout;
 		this.inputPageOffset = inputPageOffset;
 		this.outputPageOffset = outputPageOffset;
+		inputPages = new InputPage[layout.getLayout().length * layout.getLayout()[0].length];
+		outputPages = new OutputPage[layout.getInputPageCount()];
 		String numberString = "";
 		for (int i = 0; i < layout.getInputPageCount(); i++) {
 			numberString = String.format("%04d", i + inputPageOffset);
@@ -23,11 +26,37 @@ public class Signature {
 
 	public void impose() {
 
+		int inputPagesPerOutputPages = layout.getLayout()[0].length;
+		// int outputRows = layout.getOutputPageRows();
+		// int outputCols = layout.getOutputPageCols();
+		int outputRows = 2;
+		int outputCols = 2;
+
+		for (int i = 0; i < layout.getLayout().length; i++) {
+			// for each output page
+
+			// initialize the output page
+			outputPages[i] = new OutputPage("output", outputCols * inputPages[i].getWidth(),
+					outputRows * inputPages[i].getHeight());
+			for (int j = 0; j < layout.getLayout()[i].length; j++) {
+				// for each input page
+
+				// load the input page
+				inputPages[i * inputPagesPerOutputPages + j].load();
+
+				// render the input page
+				outputPages[i].addPage(0, 0, 0);
+
+				// unload the input page
+				inputPages[i].unload();
+
+			}
+		}
 	}
 
 	class InputPage {
 
-		BufferedImage page = null;
+		BufferedImage pageImage = null;
 		String fileLocation = "";
 		boolean loaded = false;
 
@@ -36,40 +65,56 @@ public class Signature {
 		}
 
 		public void load() {
-			page = FileManager.loadImage(fileLocation);
+			try {
+				pageImage = FileManager.loadImage(fileLocation);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			loaded = true;
 		}
 
 		public void unload() {
-			page = null;
+			pageImage = null;
 			loaded = false;
 		}
 
 		public boolean isLoaded() {
 			return loaded;
 		}
+
+		public int getHeight() {
+			if (!loaded) {
+				return 0;
+			}
+			return pageImage.getHeight();
+		}
+
+		public int getWidth() {
+			if (!loaded) {
+				return 0;
+			}
+			return pageImage.getWidth();
+		}
 	}
 
 	class OutputPage {
-
-		InputPage[] inputPages;
-		BufferedImage page = null;
+		BufferedImage pageImage = null;
 		String fileLocation = "";
 
-		public OutputPage(String loc, int width, int height, int inputPageCount, int pageNumber) {
+		public OutputPage(String loc, int width, int height) {
 			this.fileLocation = loc;
 		}
 
 		public void addPage(int x, int y, float rotation) {
-			
+
 		}
 
 		public void renderPage() {
-			FileManager.saveImage(page, fileLocation);
+			FileManager.saveImage(pageImage, fileLocation);
 		}
 
 		public void unload() {
-			page = null;
+			pageImage = null;
 		}
 	}
 }
